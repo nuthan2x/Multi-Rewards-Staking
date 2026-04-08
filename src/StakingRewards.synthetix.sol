ee2270e31f652ff274670851d1311eefb1d21302
nuthan2x <nuthan1159@gmail.com>
2026-04-08 13:48:01 +0530

    init

---
+++ src/StakingRewards.synthetix.sol
@@ -0,0 +1,5 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.30
+
+
+/// @notice Rewriting `synthetix/contracts/StakingRewards.sol` in  0.8.0 compiler version with latest openzeppelin lib
